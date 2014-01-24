0084cdcdd346ca9b7b077e83cbb040b38532c980
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-01-24 13:53:31 +0900

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var prefixer = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
+ 
+gulp.task('css', function () {
+  gulp.src('htdocs/css/**/*.css')
+      .pipe(plumber())
+      .pipe(prefixer('last 2 version'))
+      .pipe(concat('style.min.css'))
+      .pipe(minify())
+      .pipe(gulp.dest('htdocs/dist/css'));
+});
+ 
+gulp.task('img', function () {
+  gulp.src('htdocs/img/**/*')
+      .pipe(plumber())
+      .pipe(imagemin())
+      .pipe(gulp.dest('htdocs/dist/img'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('htdocs/css/**/*', function () {
+    gulp.run('css');
+  });
+  gulp.watch('htdocs/img/**/*', function () {
+    gulp.run('img');
+  });
+});
+ 
+gulp.task('default', function () {
+  gulp.run('css', 'img');
+});
